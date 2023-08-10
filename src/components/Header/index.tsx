ad8f96485636b3db837a5ed74b428ff41a9b9d42
elijahthis <emperorelijah007@gmail.com>
2023-08-10 13:07:19 +0100

    fix: button bug

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,12 +11,18 @@ const Header = ({ openNav, setOpenNav }: HeaderProps) => {
 	return (
 		<header className="bg-white p-4 py-3 flex flex-row items-center justify-between fixed top-0 w-full lg:hidden z-10">
 			<img src={logo} alt="" className="h-8" />
-			<button
-				className="p-2 bg-white rounded-lg"
-				onClick={() => setOpenNav(!openNav)}
-			>
-				<CgMenuLeftAlt size={24} />
-			</button>
+			{openNav ? (
+				<div className="p-2 opacity-0">
+					<CgMenuLeftAlt size={24} />
+				</div>
+			) : (
+				<button
+					className={`p-2 bg-white rounded-lg`}
+					onClick={() => setOpenNav(true)}
+				>
+					<CgMenuLeftAlt size={24} />
+				</button>
+			)}
 		</header>
 	);
 };
